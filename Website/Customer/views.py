f13ceb6d7d0dda21bc52bec741964fe4ce2141b5
Christian <papjuhles@gmail.com>
2017-07-28 13:19:05 +0200

    Update newOrders template

--- Website/Customer/views.py
+++ Website/Customer/views.py
@@ -20,7 +20,10 @@ def newOrders(request):
     else:
         form = CustomerForm()
 
-    return render(request, 'newOrders.html', {'form': form})
+    articelID = Orders.objects.only("article_id")
+    context = {'articleID': articleID,}
+    template = = loader.get_template('newOrders.html')
+    return render(template.render(context, request), {'form': form})
 
 def overview(request):
     return render(request, 'overview.html')
